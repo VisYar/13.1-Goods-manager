b8f73d8952ca795bdecc4a60b486dd6cd5e91c7c
Tatyana Dmitrieva <visyar76@gmail.com>
2022-07-28 20:50:37 +0300

    Added autotests for this functionality

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -5,23 +5,21 @@ import ru.netology.product.Product;
 public class ProductRepository {
     private Product[] items = new Product[0];
 
-    public void save(Product item) {
-        Product[] tmp = new Product[items.length + 1];
-        System.arraycopy(items, 0, tmp, 0, items.length);
-        tmp[tmp.length - 1] = item;
-        items = tmp;
-    }
+//    public void save(Product item) {
+//        Product[] tmp = new Product[items.length + 1];
+//        System.arraycopy(items, 0, tmp, 0, items.length);
+//        tmp[tmp.length - 1] = item;
+//        items = tmp;
+//    }
 
     public Product[] findAll() {
         return items;
     }
 
     public void removeById(int id) {
-        Product draft_2 = findById(id);
-        if (draft_2 == null) {
-            throw new NotFoundException(
-                    "Element with id: " + id + " not found"
-            );
+        Product draft_1 = findById(id);
+        if (draft_1 == null) {
+            throw new NotFoundException("Element with id: " + id + " not found");
         }
         Product[] tmp = new Product[items.length - 1];
         int index = 0;
@@ -34,18 +32,16 @@ public class ProductRepository {
         items = tmp;
     }
 
-    public void addNewProducts(Product id) {
-        Product draft_1 = findById(id.getId());
-        if (draft_1 != null) {
-            throw new AlreadyExistsException(
-                    "Element with id: " + id.getId() + " already exists"
-            );
+    public void addNewProducts(Product addedProduct) {
+        Product draft_2 = findById(addedProduct.getId());
+        if (draft_2 != null) {
+            throw new AlreadyExistsException("Element with id: " + addedProduct.getId() + " already exists");
         }
         Product[]tmp = new Product[items.length + 1];
         for (int i = 0; i < items.length; i++) {
             tmp[i] = items[i];
         }
-        tmp[tmp.length - 1] = id;
+        tmp[tmp.length - 1] = addedProduct;
         items = tmp;
     }
 
@@ -58,12 +54,7 @@ public class ProductRepository {
         return null;
     }
 
-    public void addNewProductIdWhichAlreadyExists(Product productForAdd) {
-        Product[]tmp = new Product[items.length + 1];
-        for (int i = 0; i < items.length; i++) {
-            tmp[i] = items[i];
-        }
-        tmp[tmp.length - 1] = productForAdd;
-        items = tmp;
+    public Product[] getSave() {
+        return items;
     }
 }
--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,12 +1,16 @@
 package ru.netology.repository;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.product.Book;
 import ru.netology.product.Product;
 import ru.netology.product.Smartphone;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ProductRepositoryTest {
+
+    ProductRepository repository = new ProductRepository();
 
     Product item1 = new Book(1, "Название_1", 100, "Автор_1");
     Product item2 = new Book(2, "Название_2", 200, "Автор_2");
@@ -15,29 +19,61 @@ public class ProductRepositoryTest {
     Product item5 = new Smartphone(5, "Название_5", 20_000, "Производитель_5");
 
     @Test
-    public void shouldRemoveExistingItemFromArray() {
-        ProductRepository repository = new ProductRepository();
-        repository.save(item1);
-        repository.save(item2);
-        repository.save(item3);
+    public void successOfDeletingExistingItem() {
+
+        repository.addNewProducts(item1);
+        repository.addNewProducts(item2);
+        repository.addNewProducts(item3);
+        repository.addNewProducts(item4);
+        repository.addNewProducts(item5);
         repository.removeById(3);
 
-        Product[] expected = {item1, item2};
-        Product[] actual = repository.findAll();
+        Product[] expected = {item1, item2, item4, item5};
+        Product[] actual = repository.getSave();
 
-        Assertions.assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldAddNonExistingItemToArray() {
-        ProductRepository repository = new ProductRepository();
-        repository.save(item1);
-        repository.save(item2);
-        repository.save(item5);
+    public void tryToRemoveNonExistentItem() {
 
-        Product[] expected = {item1, item2, item5};
-        Product[] actual = repository.findAll();
+        repository.addNewProducts(item1);
+        repository.addNewProducts(item2);
+        repository.addNewProducts(item3);
+        repository.addNewProducts(item4);
+        repository.addNewProducts(item5);
 
-        Assertions.assertArrayEquals(expected, actual);
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(7);
+        });
+    }
+
+    @Test
+    public void successOfDeletingExistingItem2() {
+
+        repository.addNewProducts(item1);
+        repository.addNewProducts(item2);
+        repository.addNewProducts(item3);
+        repository.addNewProducts(item4);
+        repository.addNewProducts(item5);
+
+        Product[] expected = {item1, item2, item3, item4, item5};
+        Product[] actual = repository.getSave();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void tryAddItemWithDuplicateId() {
+
+        repository.addNewProducts(item1);
+        repository.addNewProducts(item2);
+        repository.addNewProducts(item3);
+        repository.addNewProducts(item4);
+        repository.addNewProducts(item5);
+
+        assertThrows(AlreadyExistsException.class, () -> {
+            repository.addNewProducts(item3);
+        });
     }
 }
